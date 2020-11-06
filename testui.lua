9592411c41579ed68d993225fda0ce41bd2de283
VoidMasterX <71610591+VoidMasterX@users.noreply.github.com>
2020-11-06 13:50:26 -0600

    Update testui.lua

--- testui.lua
+++ testui.lua
@@ -5,13 +5,14 @@ function Library:Create(options)
 		highlightColor = Color3.fromRGB(0, 165, 255)
 	}
 	local windowItems = {}
-	
+
 	local UILib = Instance.new("ScreenGui")
 	local MainFrame = Instance.new("Frame")
+	local UICorner = Instance.new("UICorner")
 	local HeaderLine = Instance.new("Frame")
 	local TabContainer = Instance.new("Frame")
 	local UIListLayout = Instance.new("UIListLayout")
-	
+
 	UILib.Name = "UILib"
 	UILib.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 	UILib.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -23,6 +24,10 @@ function Library:Create(options)
 	MainFrame.Position = UDim2.new(0.2910496, 0, 0.22760801, 0)
 	MainFrame.Size = UDim2.new(0, 500, 0, 400)
 	
+	UICorner.Parent = MainFrame
+	UICorner.CornerRadius = UDim.new(0, 8)
+	UICorner.Archivable = true
+
 	HeaderLine.Name = "HeaderLine"
 	HeaderLine.Parent = MainFrame
 	HeaderLine.BackgroundColor3 = options.highlightColor
@@ -34,11 +39,11 @@ function Library:Create(options)
 	TabContainer.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	TabContainer.BackgroundTransparency = 1.000
 	TabContainer.Size = UDim2.new(0, 500, 0, 30)
-	
+
 	UIListLayout.Parent = TabContainer
 	UIListLayout.FillDirection = Enum.FillDirection.Horizontal
 	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	
+
 	local UIS = game:GetService("UserInputService")
 	local dragToggle = nil
 	local dragSpeed = 0
@@ -76,21 +81,21 @@ function Library:Create(options)
 			updateInput(input)
 		end
 	end)
-	
+
 	function windowItems:Toggle()
 		MainFrame.Visible = not MainFrame.Visible
 	end
-	
+
 	function windowItems:Tab(name)
 		name = name or "tab"
 		local tabItems = {}
-		
+
 		local Page1 = Instance.new("TextButton")
 		local Page1_2 = Instance.new("Frame")
 		local ScrollingFrame = Instance.new("ScrollingFrame")
 		local UIPadding = Instance.new("UIPadding")
 		local UIListLayout_2 = Instance.new("UIListLayout")
-		
+
 		Page1.Name = name .. "Page"
 		Page1.Parent = TabContainer
 		Page1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -100,7 +105,7 @@ function Library:Create(options)
 		Page1.Text = name
 		Page1.TextColor3 = Color3.fromRGB(255, 255, 255)
 		Page1.TextSize = 18.000
-		
+
 		Page1_2.Name = name .. "Page"
 		Page1_2.Parent = MainFrame
 		Page1_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -118,8 +123,8 @@ function Library:Create(options)
 		ScrollingFrame.BottomImage = ""
 		ScrollingFrame.CanvasSize = UDim2.new(0, 0, 2, 0)
 		ScrollingFrame.ScrollBarThickness = 3
-        ScrollingFrame.TopImage = ""
-        ScrollingFrame.Visible = false
+		ScrollingFrame.TopImage = ""
+		ScrollingFrame.Visible = false
 
 		UIPadding.Parent = ScrollingFrame
 		UIPadding.PaddingBottom = UDim.new(0, 5)
@@ -130,24 +135,24 @@ function Library:Create(options)
 		UIListLayout_2.Parent = ScrollingFrame
 		UIListLayout_2.HorizontalAlignment = Enum.HorizontalAlignment.Center
 		UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
-        UIListLayout_2.Padding = UDim.new(0, 10)
-        
-        Page1.MouseButton1Click:Connect(function()
-            for i,v in pairs(MainFrame:GetChildren()) do
-                if (v.Name:find("Page")) then
-                    if (v.Name ~= name .. "Page") then
-                        v.ScrollingFrame.Visible = false
-                    else
-                        v.ScrollingFrame.Visible = true
-                    end
-                end
-            end
-        end)
-		
+		UIListLayout_2.Padding = UDim.new(0, 10)
+
+		Page1.MouseButton1Click:Connect(function()
+			for i,v in pairs(MainFrame:GetChildren()) do
+				if (v.Name:find("Page")) then
+					if (v.Name ~= name .. "Page") then
+						v.ScrollingFrame.Visible = false
+					else
+						v.ScrollingFrame.Visible = true
+					end
+				end
+			end
+		end)
+
 		function tabItems:Section(sectionName)
 			local Section = Instance.new("Frame")
 			local Text = Instance.new("TextLabel")
-			
+
 			Section.Name = "Section"
 			Section.Parent = ScrollingFrame
 			Section.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
@@ -165,9 +170,28 @@ function Library:Create(options)
 			Text.TextSize = 18.000
 		end
 		
+		function tabItems:Button(buttonName, callback)
+			buttonName = buttonName or "Button"
+			callback = callback or function()
+				print("Button Pressed")
+			end
+			
+			local Button = Instance.new("TextButton")
+			
+			Button.Name = "Button"
+			Button.Parent = ScrollingFrame
+			Button.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+			Button.Position = UDim2.new(0.0199999996, 0, 0.01369863, 0)
+			Button.Size = UDim2.new(0, 480, 0, 30)
+			Button.Font = Enum.Font.SourceSans
+			Button.Text = "Button Info"
+			Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+			Button.TextSize = 18.000
+		end
+
 		return tabItems
 	end
-	
+
 	return windowItems
 end
 
