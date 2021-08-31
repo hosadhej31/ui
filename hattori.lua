1517c811cd32875f8aa1d56902ec155e9f975cc2
Siper <71610591+VoidMasterX@users.noreply.github.com>
2021-08-31 04:20:31 -0500

    Update hattori.lua

--- hattori.lua
+++ hattori.lua
@@ -221,7 +221,7 @@ function library:CreateTab(name, list, size, pos)
 	if pos then
 		part.Position = pos
     else
-        part.Position = UDim2.new(0, #tabs == 0 and 50 or tabs[#tabs].Position.X.Offset + 205, 0, 25)
+        part.Position = UDim2.new(0, #tabs == 0 and 25 or tabs[#tabs].Position.X.Offset + 200, 0, 25)
     end
 
 	if size then
