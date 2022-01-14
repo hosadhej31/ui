ad8243b854a6d7be0b2df04bd12e39f05e306299
Siper <71610591+VoidMasterX@users.noreply.github.com>
2022-01-14 13:00:24 -0600

    Update uwuware.lua

--- uwuware.lua
+++ uwuware.lua
@@ -284,7 +284,7 @@ function createToggle(option, parent)
 		end
 		self.callback(state)
 	end
-	if option.state and type(option.state) == "boolean" then
+	if option.state ~= nil and type(option.state) == "boolean" then
 		option.callback(option.state)
 	end
 	
